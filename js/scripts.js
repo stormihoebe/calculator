a57291a53be315caa8e1cfa780517bf7b129a7f0
Colin Bloom and Stormi Hoebelheinrich <colin@>
2017-02-15 11:39:26 -0800

    add js and css directories, add html form, programmed for to add numbers and display result

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-var add = function(number1, number2){
+//Business (or back-end) logic;
+
+var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+// Everything below this line is user interface (of front-end) logic:
+
+$(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+
+		$("#output").text(result);
+	});
+});
