6e9e7319bc8eb550887e5d8287dc495d2303b60c
Colin Bloom and Stormi Hoebelheinrich <colin@>
2017-02-15 12:00:46 -0800

    add subtract form with functionality

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 // Everything below this line is user interface (of front-end) logic:
-
+//start add
 $(function() {
 	$("form#add").submit(function(event) {
 		event.preventDefault();
@@ -25,6 +25,18 @@ $(function() {
 		var number2 = parseInt($("#add2").val());
 		var result = add(number1, number2);
 
-		$("#output").text(result);
+		$("#outputAdd").text(result);
 	});
+	//end add
+	//start subtract
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+
+		$("#outputSubtract").text(result);
+	});
+	//end subtract
 });
